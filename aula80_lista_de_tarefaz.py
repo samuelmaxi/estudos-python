30ef7e997e4e5553684828b586f699854ac1edbb
samuelmaxi <samuelgaraujo87@gmail.com>
2023-06-12 18:21:12 -0300

    starting functions (add)

--- aula80_lista_de_tarefaz.py
+++ aula80_lista_de_tarefaz.py
@@ -16,19 +16,47 @@
 # Refazer
 
 
-def add_task(task, list_todo=None):
-    if list_todo is None:
-        list_todo = []
-    list_todo.append(task)
+while True:
+    print("1- Add | 2- Undo | 3- Redo | 4-List | 5- Quit")
+    option = int(input("Enter a number from the option above: "))
+
+    if option == 1:
+        print("add".upper())
+
+        def add_task(item, list_todo):
+            list_todo.append(item)
+            return list_todo
+
+        todo = []
+
+        task = input("Typed your task today: ")
+        todo_new = add_task(task, todo)
+        add_task(task, todo_new)
+
+        continue
+    elif option == 2:
+        print("undo".upper())
+    elif option == 3:
+        print("redo".upper())
+    elif option == 4:
+        print("list".upper())
+        print(todo)
+    elif option == 5:
+        print("quit".upper())
+        break
+
+
+def add_task(item, list_todo):
+    list_todo.append(item)
     return list_todo
 
 
 todo = []
 
-task = input("Trype your taks: ")
-todo_list = add_task(task, todo)
-add_task("JANTAR", todo_list)
-add_task("comer", todo_list)
-add_task("dormir", todo_list)
+i = 1
+while i <= 2:
+    task = input("Typed your task today: ")
+    todo = add_task(task, todo)  # aqui que ta adicionand o lista
+    i += 1
 
-print(todo_list)
+print(todo)
